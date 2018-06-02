a64445227a9960a94cb6d368f31cb1ff8e4b1067
walfud <hi@walfud.com>
2018-06-02 20:39:42 +0800

    .

--- app/note/index.js
+++ app/note/index.js
@@ -1,6 +1,7 @@
 import React, {
     Component,
-} from "react"
+} from 'react'
+import 'wired-elements'
 
 export default class App extends React.Component {
     constructor() {
@@ -15,10 +16,14 @@ export default class App extends React.Component {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-                <p>
-                    <span>mac</span>
-                    <span>417286111</span>
-                </p>
+                <wired-card>
+                    <h4>Anydesk</h4>
+                    <wired-listbox>
+                        <wired-item text="home: desktop-c4cqn3p@ad" />
+                        <wired-item text="office: 586822336" />
+                        <wired-item text="mac: 417286111" />
+                    </wired-listbox>
+                </wired-card>
             </div>
         )
     }
